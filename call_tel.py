55c1ef05e5bf8b7d4efff46cf041bdce86912adf
Konstantin <saykovkon@gmail.com>
2019-02-26 18:41:31 +0300

    init commit

---
+++ call_tel.py
@@ -0,0 +1,12 @@
+def call_trid(x,x1):
+    if x == 343:
+        return x1*15
+    elif x == 381:
+        return x1*18
+    elif x == 473:
+        return x1*13
+    elif x == 485:
+        return x1*11
+    else:
+        return 'Введен неккоректный код города,попробуйте еще раз!1!11!!!'
+    
---
+++ get_parity.py
@@ -0,0 +1,6 @@
+def oDD(x):
+    if x%2 == 0:
+        return "Четное число"
+    else:
+        return "Нечетное число"
+    
