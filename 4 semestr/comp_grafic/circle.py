7564b5219751449e479ac87ea713a0a214cdbd79
Konstantin <saykovkon@gmail.com>
2020-02-18 01:03:03 +0300

    dzk_2

---
+++ 4 semestr/comp_grafic/circle.py
@@ -0,0 +1,20 @@
+import random
+from PIL import Image, ImageDraw #Подключим необходимые библиотеки
+import matplotlib.pyplot as plt
+
+image = Image.open("files/roof.jpg") #Открываем изображение
+draw = ImageDraw.Draw(image) #Создаем инструмент для рисования
+width  = image.size[0] #Определяем ширину
+height = image.size[1] #Определяем высоту
+pix = image.load() #Выгружаем значения пикселей
+for x in range(1, width, 80):
+    for y in range(360, 720 ,1):
+        for x1 in range(x,x+40):
+            draw.point((x1,y),(0,255,0))
+for x in range(width ):
+        for y in range(height):
+               if (x-720)**2+(y-540)**2 <= 300**2:
+                   draw.point((x,y),(249,166,2) )
+
+image.save("lab_1/cirle_roof.jpg")
+del draw
