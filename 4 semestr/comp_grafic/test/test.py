bcff3c7b954c444287ade662444f3e238a30e8b9
Konstantin <saykovkon@gmail.com>
2020-02-18 20:12:56 +0300

    dzk_2

---
+++ 4 semestr/comp_grafic/test/test.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageDraw
+
+image = Image.open("../files/roof.JPG")
+draw = ImageDraw.Draw(image)
+width  = image.size[0]
+height = image.size[1]
+for x in range(80, width, 160):
+    for y in range(height):
+        for x1 in range(x,x+80):
+            draw.point((x1,y),(0,255,128))
+image.show()
+image.save("first.jpg")
+del draw
---
+++ 4 semestr/comp_grafic/test/test_2.py
@@ -0,0 +1,17 @@
+from PIL import Image, ImageDraw
+
+image = Image.open("../files/roof.JPG")
+draw = ImageDraw.Draw(image)
+width  = image.size[0]
+height = image.size[1]
+pix = image.load()
+for x in range(width ):
+        for y in range(height):
+               if (x)**2+(y-540)**2 <= 540**2:
+                   a = pix[x, y][0]
+                   b = pix[x, y][1]
+                   c = pix[x, y][2]
+                   draw.point((x,y),(255-a,255-b, 255-c))
+image.show()
+image.save("second.jpg")
+del draw
