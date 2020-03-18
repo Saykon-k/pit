ec0ce7d47242be54f4b8ba8aa52879eb129112a3
Konstantin <saykovkon@gmail.com>
2020-03-18 12:09:09 +0300

    fix.,,

--- 4 semestr/c-prog/dz_from_17_to_23_04_20/7_palindrom.c
+++ 4 semestr/c-prog/dz_from_17_to_23_04_20/7_palindrom.c
@@ -2,23 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 #include "math.h"
-#define N 3 /* количество элементов массива */
+#define N 10 /* количество элементов массива */
 
 int main()
 {
   int a[N]; /* массив a размера N */
   int i;    /* счетчик */
   srand(time(NULL)); /* начальное значение генератора ПСЧ */
-  for(i = 0; i < N/2; i++){
-    a[i] = i;
+  // for(i = 0; i < N/2; i++){
+  //   a[i] = i;
+  // }
+  // for(i = (int) N/2; i < N ; i++){
+  //   a[i] = (sizeof(a)/4)-i-1;
+  // } this code for generate mass-polindom
+  for(i = 0; i < N; i++){
+    a[i] = rand()%100;
+    printf("%d , ", a[i]);
   }
-  a[1] = 1;
-  a[2] = 0;
-  int half_len_mas = sizeof(N)/4;
+  printf("\n");
+  int half_len_mas = (int) sizeof(a)/4-1;
   if(N  % 2 == 0){
     for(i = 0; i < half_len_mas/2 ; i++ ){
-      printf("%d\n", a[i] != a[half_len_mas-i]);
+      printf("a[i] == %d a[half_len_mas-i] == %d\n", a[i],a[half_len_mas-i]);
     if(a[i] != a[half_len_mas-i]){
+      printf("a[i] == %d  != a[half_len_mas-i]  == %d\n", a[i],a[half_len_mas-i]);
+
       printf("ne polindrom\n");
       i = half_len_mas;
     }
