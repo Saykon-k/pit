633605a8d4f81bb71ba909af389a75fe5eaae8fc
Konstantin <saykovkon@gmail.com>
2020-03-23 12:59:11 +0300

    fix

--- 4 semestr/c-prog/dz_from_17_to_23_04_20/11_double_string.c
+++ 4 semestr/c-prog/dz_from_17_to_23_04_20/11_double_string.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void double_ke(char *s){
+void double_ke(char *str){
   int i = 0 ;
-  while(s[i] != '\0'){
+  while(str[i] != '\0'){
     i++;
   }
-  char le[100] = "";
-  strcat(le,s);
-  strcat(s,le);
+  int k = 0;
+  while(k < i){
+    str[k+i] = str[k];
+    k++;
+  }
 }
 
 int main() {
@@ -16,5 +18,6 @@ int main() {
   scanf("%39s", str);
   double_ke(str);
   printf("%s\n",str );
+   scanf("%39s", str);
   return 0;
 }
