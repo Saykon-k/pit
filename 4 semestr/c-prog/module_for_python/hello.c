de1691ade11f31e0a2a2257a6ead70bf185e6701
Konstantin <saykovkon@gmail.com>
2020-05-03 03:05:42 +0300

    ну чет есть...

--- 4 semestr/c-prog/module_for_python/hello.c
+++ 4 semestr/c-prog/module_for_python/hello.c
@@ -73,6 +73,59 @@ static PyObject* pow_complex(PyObject *self, PyObject *args) {
     Py_RETURN_NONE;
 }
 
+static PyObject *py_expected_value(PyObject *self, PyObject *args) {
+    PyObject *pylist1, *pylist2;
+    double  double_item1, double_item2, len1, len2;
+    double result = 0;
+    double* list1;
+    double* list2;
+    double* inter;
+    int i,i1;
+    int i2 = -1;
+
+    if (!PyArg_ParseTuple(args,"OO",&pylist1, &pylist2)) {
+        return NULL;
+    }
+    len1 = PySequence_Fast_GET_SIZE(pylist1);
+    len2 = PySequence_Fast_GET_SIZE(pylist2);// определяем размер
+
+    list1 = (double*)malloc(len1 * sizeof(double));
+    list2 = (double*)malloc(len2 * sizeof(double));// тут юзаем динамическую память, потому что дефолтная не катит
+    if(len1<len2){
+      inter = (double*)malloc(len1 * sizeof(double));
+    }else{
+      inter = (double*)malloc(len2 * sizeof(double));
+    }
+    for(i=0; i<len1; i++) {
+        PyObject *item1 = PySequence_Fast_GET_ITEM(pylist1,i);
+        double_item1 = PyFloat_AsDouble(item1);
+        list1[i] = double_item1;
+      //  printf("%lf\n", list1[i] );
+    }//эт заполняет первый массив
+    for(i=0; i<len2; i++) {
+        PyObject *item2 = PySequence_Fast_GET_ITEM(pylist2,i);
+        double_item2 = PyFloat_AsDouble(item2);
+        list2[i] = double_item2;
+      //  printf("%lf\n", list2[i] );
+    }// эт заполняет второй массив
+    for(i = 0; i<len1;i++){
+      for(i1 = 0; i1<len2;i1++){
+        printf("f\n");
+        if(list1[i]== list2[i1]){
+          //printf("%d\n",i );
+          i2++;
+          inter[i2] = list1[i];
+        }
+      }
+    }
+    for(i = 0; i<= sizeof(inter)/8;i++){//воспомнить,какой критерий остановки динамического массива
+      printf("%lf\n", inter[i]);
+    }
+    return Py_BuildValue("d", result);
+}
+
+/* here is how you expose it to Python code: */
+
 
 // Method definition object for this extension, these argumens mean:
 // ml_name: The name of the method
@@ -81,6 +134,7 @@ static PyObject* pow_complex(PyObject *self, PyObject *args) {
 //          accepting arguments, accepting keyword arguments, being a
 //          class method, or being a static method of a class.
 // ml_doc:  Contents of this method's docstring
+//какое-то оформление
 static PyMethodDef hello_methods[] = {
     {
         "subtraction", subtraction, METH_VARARGS,
@@ -100,6 +154,10 @@ static PyMethodDef hello_methods[] = {
     },
     {
         "pow_complex", pow_complex, METH_VARARGS,
+        "Print 'hello xxx' from a method defined in a C extension."
+    },
+    {
+        "py_expected_value", py_expected_value, METH_VARARGS,
         "Print 'hello xxx' from a method defined in a C extension."
     },
     {NULL, NULL, 0, NULL}
