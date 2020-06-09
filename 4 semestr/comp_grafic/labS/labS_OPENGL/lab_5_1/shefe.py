30f9860cde2c2baba5bb392d40854b4c5d8d5acd
Konstantin <saykovkon@gmail.com>
2020-06-09 18:48:14 +0300

    rename

---
+++ 4 semestr/comp_grafic/labS/labS_OPENGL/lab_5_1/shefe.py
@@ -0,0 +1,65 @@
+from math import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+
+# Процедура инициализации
+def init():
+    glEnable(GL_DEPTH_TEST)
+    glClearColor(0.5, 0.5, 0.5, 1.0)  # Серый цвет для первоначальной закраски
+    gluOrtho2D(-1.0, 1.0, -1.0, 1.0)  # Определяем границы рисования по горизонтали и вертикали
+    global filled
+    filled = 1
+
+
+# Процедура обработки обычных клавиш
+def keyboardkeys(key, x, y):
+    global filled
+    if key == b'\x1b':
+        sys.exit(0)
+    if key == b' ':
+        filled = 1 - filled
+    glutPostRedisplay()  # Вызываем процедуру перерисовки
+
+
+# Процедура рисования
+def draw(*args, **kwargs):
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # Очищаем экран и заливаем текущим цветом фона
+    glRotated(0.125, 1, 1, 1)
+    global filled
+    if filled == 1:
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+    else:
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+    R = 0.5
+
+    for j in range(-9, 9):
+        glColor3f(1, 1, 1)
+        glBegin(GL_QUAD_STRIP)
+        for i in range(21):
+            glVertex3d(R * cos(pi * j / 18) * cos(2 * pi * i / 20),
+                       R * cos(pi * j / 18) * sin(2 * pi * i / 20),
+                       R * sin(pi * j / 18))
+            glVertex3d(R * cos(pi * (j + 1) / 18) * cos(2 * pi * i / 20),
+                       R * cos(pi * (j + 1) / 18) * sin(2 * pi * i / 20),
+                       R * sin(pi * (j + 1) / 18))
+        glEnd()
+
+    glutSwapBuffers()  # Меняем буферы
+    glutPostRedisplay()  # Вызываем процедуру перерисовки
+
+
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+glutInitWindowSize(800, 600)
+glutInitWindowPosition(50, 50)
+glutInit(sys.argv)
+glutCreateWindow(b"OpenGL Second Program!")
+# Определяем процедуру, отвечающую за рисование
+glutDisplayFunc(draw)
+# Определяем процедуру, отвечающую за обработку обычных клавиш
+glutKeyboardFunc(keyboardkeys)
+# Вызываем нашу функцию инициализации
+init()
+glutMainLoop()
