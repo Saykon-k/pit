92d8cb1a1743b419ae795bbf52a1fac1921c7aff
Konstantin <saykovkon@gmail.com>
2020-10-29 22:13:11 +0300

    del....

--- 4_semester/some_prog_for_exam/class_mini/e.py
+++
@@ -1,8 +0,0 @@
-class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-    def print_data(self):
-        print(str(self.height)+" "+ str(self.height))
-obj  = Rectangle(12,12)
-obj.print_data()
--- 4_semester/some_prog_for_exam/cvs_mini/pandas/use_pandas.py
+++
@@ -1,7 +0,0 @@
-import pandas
-df  = pandas.read_csv('hrdata.csv')
-print(df)
-print()
-print()
-df  = pandas.read_csv('hrdata.csv',index_col='name',parse_dates=['Hire Date'])
-print(df)
