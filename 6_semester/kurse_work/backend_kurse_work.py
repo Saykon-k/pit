86cf09af51d00d6e2d9b8b739de345efcbf99085
saykovkon <saykovkon@gmail.com>
2021-05-04 15:53:56 +0300

    find_max_path

--- 6_semester/kurse_work/backend_kurse_work.py
+++ 6_semester/kurse_work/backend_kurse_work.py
@@ -1,4 +1,6 @@
 import copy
+# кароч 1 связи, которые проставил пользователь ниже функция пример
+# выходной список - список индексов, по которым нужно будет строить граф - учебник фрола - сошлось - ок
 def determite_work(connection_from_user):
     rank_table = []
     print(connection_from_user)
@@ -28,8 +30,8 @@ def determite_work(connection_from_user):
                             last_rank = i1
 
             if check_index_valide == len(connection_from_user[i]):
-                print(connection_from_user[i])
-                print(i)
+                # print(connection_from_user[i])
+                # print(i)
                 if not last_rank + 1 in info_about_rank.keys():
                     info_about_rank[last_rank + 1] = set()
                     info_about_rank.get(last_rank + 1).add(i)
@@ -37,19 +39,68 @@ def determite_work(connection_from_user):
                     info_about_rank.get(last_rank + 1).add(i)
             else:
                 prom.append(i)
-            print('----')
+            # print('----')
 
             # print(info_about_rank)
                 # print(last_rank)
                 # print(j)
                 # print()
         # print(info_about_rank)
-        for i1,i in info_about_rank.items():
-            print(i1, '->', i)
+
         not_none_index = copy.deepcopy(prom)
         prom = []
+    for i1, i in info_about_rank.items():
+        print(i1, '->', i)
+    return info_about_rank
+
+def find_t_and_T(connection_from_user, ranked_info, info_about_work):
+    prom_info_date = []
+    dict_info_mini_t = {}
+    dict_info_big_T = {}
+    k = 0
+    for i in ranked_info.values():
+        prom_info = []
+        for j in i:
+            if connection_from_user[j][0] == -1:
+                dict_info_mini_t[f't_{k}'] = info_about_work[j]
+                dict_info_big_T[f'T_{k}'] = info_about_work[j]
+                k +=1
+            else:
+                dict_info_mini_t[f't_{k}'] = info_about_work[j]
+                prom_list = [dict_info_big_T.get(f'T_{i}') for i in connection_from_user[j]]
+                # # for i in connection_from_user:
+                # if f'T_{k}' == 'T_11':
+                #     print('------')
+                #     print(dict_info_mini_t)
+                #     print(connection_from_user[j])
+                #     print(prom_list)
+                #     print('------')
+                dict_info_big_T[f'T_{k}'] = info_about_work[j] + max(prom_list)
+                prom_info.append(max([info_about_work[i] for i in connection_from_user[j]]) + info_about_work[j])
+                k += 1
+        prom_info_date.append(copy.deepcopy(prom_info))
+        prom_info.clear()
+    # print(dict_info_big_T)
+    # print(dict_info_mini_t)
+    return [dict_info_mini_t, dict_info_big_T]
+
+def find_max_path(connection_from_user, info_about_T, info_):
+    max_path = -1
+    key_max = -1
+    for i, j in info_about_t_and_T.items():
+        if j > max_path:
+            max_path = j
+            key_max = i
+    print(key_max)
+
+def main(connection_from_user, info_about_work):
+    ranked_info = determite_work(connection_from_user)
+    print('find_max_path')
+    info_about_t_and_T = find_t_and_T(connection_from_user, ranked_info, info_about_work)
+    find_max_path(connection_from_user, info_about_t_and_T)
 
 
-determite_work([[-1],[0,2],[-1],[0,1,2],[-1],[-1],[0,3,9],[0,1],[2,3,4],[8],[6,11],[0,1],[3,4,9],[2,3,4],[9],[6]])
-# a  = 1
-# print(a)
+# main([[-1],[0,2],[-1],[0,1,2],[-1],[-1],[0,3,9],[0,1],[2,3,4],[8],[6,11],[0,1],[3,4,9],[2,3,4],[9],[6]],
+#                [10, 5, 15, 10, 30, 5, 15, 25, 15, 30, 35, 10])
+main([[-1], [-1], [-1], [0, 1], [1, 2], [1, 3], [2, 4], [3, 4], [6], [5, 7], [8, 9], [9]],
+     [10, 5, 15, 10, 30, 5, 15, 25, 15, 30, 35, 10])
