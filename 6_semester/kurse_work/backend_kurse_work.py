dedfa37a9e37b8496f11fd1c6c531043dd4f05e4
saykovkon <saykovkon@gmail.com>
2021-05-06 18:21:16 +0300

    fix T

--- 6_semester/kurse_work/backend_kurse_work.py
+++ 6_semester/kurse_work/backend_kurse_work.py
@@ -1,6 +1,4 @@
 import copy
-import cvxpy
-import math
 # кароч 1 связи, которые проставил пользователь ниже функция пример
 # выходной список - список индексов, по которым нужно будет строить граф - учебник фрола - сошлось - ок
 def determite_work(connection_from_user):
@@ -63,12 +61,19 @@ def find_t_and_T(connection_from_user, ranked_info, info_about_work):
         prom_info = []
         for j in i:
             if connection_from_user[j][0] == -1:
-                dict_info_t[f't_{k}'] = info_about_work[j]
-                dict_info_t[f'T_{k}'] = info_about_work[j]
-                k +=1
+                dict_info_t[f't_{j}'] = info_about_work[j]
+                dict_info_t[f'T_{j}'] = info_about_work[j]
+                k += 1
             else:
+                print('---------')
+                print(i)
+                print(j)
+                print(dict_info_t)
+                print(connection_from_user[j])
                 dict_info_t[f't_{k}'] = info_about_work[j]
                 prom_list = [dict_info_t.get(f'T_{i}') for i in connection_from_user[j]]
+                print('---------')
+
                 # # for i in connection_from_user:
                 # if f'T_{k}' == 'T_11':
                 #     print('------')
@@ -76,7 +81,7 @@ def find_t_and_T(connection_from_user, ranked_info, info_about_work):
                 #     print(connection_from_user[j])
                 #     print(prom_list)
                 #     print('------')
-                dict_info_t[f'T_{k}'] = info_about_work[j] + max(prom_list)
+                dict_info_t[f'T_{j}'] = info_about_work[j] + max(prom_list)
                 k += 1
         prom_info_date.append(copy.deepcopy(prom_info))
         prom_info.clear()
@@ -110,50 +115,24 @@ def find_max_path(connection_from_user, info_about_T):
         prom_path.append(number_info)
         value_to_next = number_info
 
-   return [prom_path, key_max]
+   return prom_path
    #      print('-----')
    #      print(connection_from_user[value_to_next])
    #
    #      print('-----')
    #
    # print(prom_path)
-def deterimate_all_alfa():
-    #пока неизвестно, как это сделать
-    return [0.1, 0.2, 0.05]
-# def opt_1_without_change_max_path()
-def main(connection_from_user, info_about_work, minimum_term_mass, dead_line_all_projects,money):
+
+def main(connection_from_user, info_about_work):
     ranked_info = determite_work(connection_from_user)
     print('info_about_t_and_T')
     info_about_t_and_T = find_t_and_T(connection_from_user, ranked_info, info_about_work)
     print('find_max_path')
-    path_max = find_max_path(connection_from_user, info_about_t_and_T)
-    print(path_max)
+    max_path = find_max_path(connection_from_user, info_about_t_and_T)
+    print(max_path)
 
-#
-# main([[-1],[0,2],[-1],[0,1,2],[-1],[-1],[0,3,9],[0,1],[2,3,4],[8],[6,11],[0,1],[3,4,9],[2,3,4],[9],[6]],
-#                [10, 5, 15, 10, 30, 5, 15, 25, 15, 30, 35, 10])
+
 # main([[-1], [-1], [-1], [0, 1], [1, 2], [1, 3], [2, 4], [3, 4], [6], [5, 7], [8, 9], [9]],
 #      [10, 5, 15, 10, 30, 5, 15, 25, 15, 30, 35, 10])
-# main([[-1],[-1],[-1],[0,1],[0,1,2],[0,1,2], [5],[3,4,6]],
-#      [20, 10, 5, 30, 10, 15, 10, 10],
-#      [15, 5, 5, 10, 5, 10, 5, 5],
-#      35,
-#      10
-#      )
-main([[-1],[-1],[-1],[0,1],[0,1,2],[0,1,2], [5],[3,4,6]],
-     [15, 10, 5, 10, 10, 15, 10, 5],
-     [15, 5, 5, 10, 5, 10, 5, 5],
-     35,
-     10
-     )
-# [[-1], [-1], [-1], [0, 1], [0, 1, 2], [0, 1, 2], [5], [3, 4, 6]]
-# [[0, 1], [0, 1, 2], [0, 1, 2], [5], [3, 4, 6]]
-# [3, 4, 5, 6, 7]
-# -1 -> {0, 1, 2}
-# 0 -> {3, 4, 5}
-# 1 -> {6}
-# 2 -> {7}
-# info_about_t_and_T
-# {'t_0': 20, 'T_0': 20, 't_1': 10, 'T_1': 10, 't_2': 5, 'T_2': 5, 't_3': 30, 'T_3': 50, 't_4': 10, 'T_4': 30, 't_5': 15, 'T_5': 35, 't_6': 10, 'T_6': 45, 't_7': 10, 'T_7': 60}
-# find_max_path
-# [[7, 3, 0], 'T_7']
+main([[-1],[-1],[-1],[0],[0],[2],[1, 4, 5], [1, 4, 5],[3, 6], [2], [1, 3, 5,9]],
+     [8, 10, 6, 9, 5, 2, 4, 13, 8, 17, 10])
